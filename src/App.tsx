e88997068a910159c8e40e7b8151d670120e93da
Narathip <narathip2002@outlook.co.th>
2024-07-27 21:44:00 +0700

    init project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import BlurIn from "@/components/magicui/blur-in";
+
+const App = () => {
+  return (
+    <div>
+      <BlurIn word={"Hello World"}></BlurIn>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+// styles
+import "@/styles/globals.css";
+
+import App from "./App.tsx";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
